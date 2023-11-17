14ffe4c778393e2ead64878f9ab645b31b93541a
shubham0711-crypto <shubhamchoksi15@gmail.com>
2023-11-17 11:31:27 -0500

    first commit

---
+++ UI/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from datetime import datetime
+from flask import render_template
+
+
+app = Flask(__name__)
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
